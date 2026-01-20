e6a9f5c60f28ddc3858422dba3a734cbcbb6d5d6
smokingkrillz <89337823+smokingkrillz@users.noreply.github.com>
2026-01-20 16:26:00 +0100

    2 c

---
+++ going_broke.py
@@ -0,0 +1,50 @@
+import random
+import numpy as np
+
+symbols = ["BAR", "BELL", "LEMON", "CHERRY"]
+
+
+def slot_machine():
+    return [random.choice(symbols) for _ in range(3)]
+
+
+def payout(s):
+
+    if s[0] == s[1] == s[2] and s[0] == "BAR":
+        return 20
+    if s[0] == s[1] == s[2] and s[0] == "BELL":
+        return 15
+    if s[0] == s[1] == s[2] and s[0] == "LEMON":
+        return 5
+    if s[0] == s[1] == s[2] and s[0] == "CHERRY":
+        return 3
+    if s[0] == s[1] and s[0] == "CHERRY":
+        return 2
+    if s[0] == "CHERRY":
+        return 1
+    return 0
+
+def plays_until_broke(start=10):
+    bankroll = start
+    plays = 0
+    while bankroll > 0:
+        s = slot_machine()
+        prize = payout(s)
+        bankroll += prize - 1
+        plays += 1
+    return plays
+
+
+total_plays = np.zeros(10000)
+# simulate
+for i in range(10000):
+    
+    plays = plays_until_broke()
+    total_plays[i] = plays
+# mean
+mean_plays = total_plays.mean()
+print("Mean plays until broke:", mean_plays)
+
+# calculate median
+median_plays = np.median(total_plays)
+print("Median plays until broke:", median_plays)
